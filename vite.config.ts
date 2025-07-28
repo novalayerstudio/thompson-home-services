d36390fb0ad777ec294fed227c92ee5e59d87b7b
Julian Quenet <julian.quenet@xibsolutoins.com>
2025-07-28 18:29:38 +0200

    Fixing the vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base:"thompson-home-services"
+  base:"thompson-home-services",
   server: {
     host: "::",
     port: 8080,
